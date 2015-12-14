a566ea7eabce6d57be31c9eba34597bc06eadb6a
yozz <yozz.online@gmail.com>
2015-12-14 15:41:46 -0500

    Enqueue names

--- startup-cpt-charts.php
+++ startup-cpt-charts.php
@@ -244,9 +244,9 @@ if ( function_exists( 'shortcode_ui_register_for_shortcode' ) ) {
 
 // Enqueue scripts and styles.
 function startup_cpt_charts_scripts() {
-    wp_enqueue_style( 'startup-cpt-charts-pizza', plugins_url( '/css/pizza.min.css', __FILE__ ), array( ), false, 'all' );
-    wp_enqueue_script( 'startup-cpt-charts-pizza', plugins_url( '/js/pizza.min.js', __FILE__ ), array( ), '', false );
-    wp_enqueue_script( 'startup-cpt-charts-pizza-dependencies', plugins_url( '/js/dependencies.js', __FILE__ ), array( ), '', false );
+    wp_enqueue_style( 'pizza', plugins_url( '/css/pizza.min.css', __FILE__ ), array( ), false, 'all' );
+    wp_enqueue_script( 'pizza', plugins_url( '/js/pizza.min.js', __FILE__ ), array( ), '', false );
+    wp_enqueue_script( 'pizza-dependencies', plugins_url( '/js/dependencies.js', __FILE__ ), array( ), '', false );
 }
 
 add_action( 'wp_enqueue_scripts', 'startup_cpt_charts_scripts' );
